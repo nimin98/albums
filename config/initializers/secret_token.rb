088b01eb1aa23d8920e0692081d1a1ff7436dae5
Min Ni <nimin98@gmail.com>
2012-04-14 12:21:14 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Albums::Application.config.secret_token = 'ba182199b809614dd06c5a4e08e24344f14875aaf0dafd9355ef99815a2edabac79d2498ced57ca5c6f1e98ceaf43fb8e0728cad8b586dd327643cc789063d6f'
